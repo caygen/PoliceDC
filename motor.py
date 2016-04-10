3d650b773ec70fe1d57df580c68b461d20d57066
byy <heyyeh12@aol.com>
2016-04-10 22:05:46 +0000

    working motor test

--- motor.py
+++ motor.py
@@ -2,39 +2,76 @@
 import RPi.GPIO as GPIO
 import time
 
-# Pin Definitons:
-pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
-ledPin = 23 # Broadcom pin 23 (P1 pin 16)
-butPin = 17 # Broadcom pin 17 (P1 pin 11)
 
-dc = 95 # duty cycle (0-100) for PWM pin
+### FUNCTIONS
+class Motor:
+    def __init__(self, A, B):
+        self.A = A
+        self.B = B
+        self.mode = "brake"
+        GPIO.setup(A, GPIO.OUT)
+        GPIO.setup(B, GPIO.OUT)
+        self.brake()
+    
+    def __str__(self):
+        return "Pin A: " + str(self.A) + ", Pin B: " + str(self.B) + ", Mode: " + self.mode
 
-# Pin Setup:
+    def cw(self):
+        GPIO.output(self.A, GPIO.HIGH)
+        GPIO.output(self.B, GPIO.LOW)
+        self.mode = "cw"
+
+    def ccw(self):
+        GPIO.output(self.A, GPIO.HIGH)
+        GPIO.output(self.B, GPIO.HIGH)
+        self.mode = "ccw"
+
+    def brake(self):
+        GPIO.output(self.A, GPIO.LOW)
+        GPIO.output(self.B, GPIO.LOW)
+        self.mode = "brake"
+
+### SETUP
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
 
+# PWM setup
+pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
+dc = 95 # duty cycle (0-100) for PWM pin
+freq = 20000
 GPIO.setup(pwmPin, GPIO.OUT) # PWM pin set as output
-pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
+pwm = GPIO.PWM(pwmPin, freq)  # Initialize PWM
+pwm.start(dc) # Initial state
 
-# Initial state for LEDs:
-GPIO.output(ledPin, GPIO.LOW)
-pwm.start(dc)
+# Motor pins
+m1 = Motor(23, 24)
+m2 = Motor(20, 21)
+m3 = Motor(13, 19)
+m4 = Motor(22, 27)
 
 print("Here we go! Press CTRL+C to exit")
 try:
     while 1:
-        GPIO.output(23, GPIO.HIGH)
-        GPIO.output(24, GPIO.HIGH)
-#        if GPIO.input(butPin): # button is released
-#            pwm.ChangeDutyCycle(dc)
-#            GPIO.output(ledPin, GPIO.LOW)
-#        else: # button is pressed:
-#            pwm.ChangeDutyCycle(100-dc)
-#            GPIO.output(ledPin, GPIO.HIGH)
-#            time.sleep(0.075)
-#            GPIO.output(ledPin, GPIO.LOW)
-#            time.sleep(0.075)
-except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+        m1.ccw()
+        m2.ccw()
+        m3.ccw()
+        m4.ccw()
+        time.sleep(5)
+        m1.cw()
+        m2.cw()
+        m3.cw()
+        m4.cw()
+        time.sleep(5)
+        #pwm.stop()
+        m1.brake()
+        m2.brake()
+        m3.brake()
+        m4.brake()
+        #pwm.start(dc)
+        time.sleep(5)
+        #pwm.ChangeDutyCycle(dc)
+
+# If CTRL+C is pressed, exit cleanly:
+except KeyboardInterrupt: 
     pwm.stop() # stop PWM
     GPIO.cleanup() # cleanup all GPIO
+
